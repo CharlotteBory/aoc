72080960a24f53c562b313b6e75921848f1c5c75
Charlotte Bory <cmmbory@gmail.com>
2022-12-02 10:20:28 +0100

    Day 2 part 2

--- 2022/02/solution.rb
+++ 2022/02/solution.rb
@@ -2,8 +2,7 @@ input_path = File.expand_path(File.dirname(__FILE__)) + "/data.txt"
 
 input = File.open(input_path).read.strip
 
-
-SCORES = {
+SCORES_PART1 = {
   "A X": 3 + 1,
   "A Y": 6 + 2,
   "A Z": 0 + 3,
@@ -15,5 +14,20 @@ SCORES = {
   "C Z": 3 + 3,
 }
 
+SCORES_PART2 = {
+  "A X": 0 + 3,
+  "A Y": 3 + 1,
+  "A Z": 6 + 2,
+  "B X": 0 + 1,
+  "B Y": 3 + 2,
+  "B Z": 6 + 3,
+  "C X": 0 + 2,
+  "C Y": 3 + 3,
+  "C Z": 6 + 1,
+}
+
 # Part 1
-p input.split("\n").map { |s| p SCORES[s.to_sym] }.sum
+p input.split("\n").map { |s| SCORES_PART1[s.to_sym] }.sum
+
+# Part 2
+p input.split("\n").map { |s| SCORES_PART2[s.to_sym] }.sum
