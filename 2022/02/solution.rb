9f640a62614791d697da4a042d8960db15905f57
Charlotte Bory <cmmbory@gmail.com>
2022-12-02 10:11:39 +0100

    Day 2 part 1

---
+++ 2022/02/solution.rb
@@ -0,0 +1,19 @@
+input_path = File.expand_path(File.dirname(__FILE__)) + "/data.txt"
+
+input = File.open(input_path).read.strip
+
+
+SCORES = {
+  "A X": 3 + 1,
+  "A Y": 6 + 2,
+  "A Z": 0 + 3,
+  "B X": 0 + 1,
+  "B Y": 3 + 2,
+  "B Z": 6 + 3,
+  "C X": 6 + 1,
+  "C Y": 0 + 2,
+  "C Z": 3 + 3,
+}
+
+# Part 1
+p input.split("\n").map { |s| p SCORES[s.to_sym] }.sum
