78ee4b79c62dbfab81bfb0322e10f23a64d9b175
Charlotte Bory <cmmbory@gmail.com>
2022-12-04 15:27:43 +0100

    Day 4 part 2

--- 2022/04/solution.rb
+++ 2022/04/solution.rb
@@ -13,11 +13,14 @@ input_path = File.expand_path(File.dirname(__FILE__)) + "/data.txt"
 input = File.open(input_path).read.strip.split("\n")
 
 input = input.map { |p| p.split(",").map { |p| p.split("-").map(&:to_i)} }
+complete_overlaps = 0
 overlaps = 0
 input.each do |pair|
   if pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1] || pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]
+    complete_overlaps += 1
+  end
+  unless pair[0][1] < pair[1][0] || pair[0][0] > pair[1][1]
     overlaps += 1
   end
 end
-p input
 p overlaps
