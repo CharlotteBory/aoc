324c4821f17c72919383191769ab7f630fcde076
Charlotte Bory <cmmbory@gmail.com>
2022-12-04 15:11:10 +0100

    Day 4 part 1

---
+++ 2022/04/solution.rb
@@ -0,0 +1,23 @@
+#  *        *        *             __o    *       *
+#   *      *       *        *    /_| _     *
+#      K  *     K      *        O'_)/ \  *    * .
+#     <')____  <')____    __*   V   \  ) __  * .
+#      \ ___ )--\ ___ )--( (    (___|__)/ /*     *
+#     *  |   |    |   |  * \ \____| |___/ /  * .
+#          |*  |    |   |    \____________/       *
+#
+#                s p o i l e r s
+
+input_path = File.expand_path(File.dirname(__FILE__)) + "/data.txt"
+
+input = File.open(input_path).read.strip.split("\n")
+
+input = input.map { |p| p.split(",").map { |p| p.split("-").map(&:to_i)} }
+overlaps = 0
+input.each do |pair|
+  if pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1] || pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]
+    overlaps += 1
+  end
+end
+p input
+p overlaps
