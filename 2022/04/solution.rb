b769d5d5ca60286d62c7bbe0dce054c87a704e8b
Charlotte Bory <cmmbory@gmail.com>
2022-12-04 15:34:08 +0100

    Use words to make sense of comparisons

--- 2022/04/solution.rb
+++ 2022/04/solution.rb
@@ -13,14 +13,22 @@ input_path = File.expand_path(File.dirname(__FILE__)) + "/data.txt"
 input = File.open(input_path).read.strip.split("\n")
 
 input = input.map { |p| p.split(",").map { |p| p.split("-").map(&:to_i)} }
+
 complete_overlaps = 0
 overlaps = 0
+
 input.each do |pair|
-  if pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1] || pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]
-    complete_overlaps += 1
-  end
-  unless pair[0][1] < pair[1][0] || pair[0][0] > pair[1][1]
-    overlaps += 1
-  end
+  # Part 1
+  first_range_includes_second = pair[0][0] >= pair[1][0] && pair[0][1] <= pair[1][1]
+  second_range_includes_first = pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]
+
+  complete_overlaps += 1 if second_range_includes_first || first_range_includes_second
+
+  # Part 2
+  lower_bounds_bigger_than_upper = pair[0][1] < pair[1][0] || pair[0][0] > pair[1][1]
+
+  overlaps += 1 unless lower_bounds_bigger_than_upper
 end
+
+p complete_overlaps
 p overlaps
