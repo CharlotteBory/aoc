4e784c063befc5cead03a697d44b95f54da3ab50
Charlotte Bory <cmmbory@gmail.com>
2022-12-05 12:39:58 +0100

    Day 5 part 1

---
+++ 2022/05/solution.rb
@@ -0,0 +1,48 @@
+#  *        *        *             __o    *       *
+#   *      *       *        *    /_| _     *
+#      K  *     K      *        O'_)/ \  *    * .
+#     <')____  <')____    __*   V   \  ) __  * .
+#      \ ___ )--\ ___ )--( (    (___|__)/ /*     *
+#     *  |   |    |   |  * \ \____| |___/ /  * .
+#          |*  |    |   |    \____________/       *
+#
+#                s p o i l e r s
+
+input_path = File.expand_path(File.dirname(__FILE__)) + "/data.txt"
+
+input = File.open(input_path).read.split("\n")
+
+starting_positions = input.take_while { |l| l.include?("[") }
+instructions = input[starting_positions.size + 2..]
+
+stack_number = input[starting_positions.size][-1].to_i
+
+stacks = starting_positions
+  .map { |s| s.ljust(stack_number * 4 - 1) }
+  .map { |s| s.gsub("    ", "[.]") }
+  .map { |s| s.gsub(" ", "") }
+  .map { |s| s.gsub("[", "").gsub("]", "") }
+  .map { |s| s.chars }
+  .transpose
+  .map(&:reverse)
+  .map { |a| a.select { |l| l != "." } }
+
+def fulfill_instruction(stacks, instruction)
+  instruction = instruction
+    .gsub("move ", "")
+    .gsub(" from ", ",")
+    .gsub(" to ", ",")
+    .split(",")
+    .map(&:to_i)
+  from = instruction[1] - 1
+  crate_nb = instruction[0]
+  to = instruction[2] - 1
+  crates = stacks[from].pop(crate_nb)
+  stacks[to].concat(crates.reverse)
+  stacks
+end
+
+instructions.each { |i| stacks = fulfill_instruction(stacks, i) }
+
+
+p stacks.map(&:last).join
