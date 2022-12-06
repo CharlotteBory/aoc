14333b2a727ef3abfb91864fdac38e56cb938063
Charlotte Bory <cmmbory@gmail.com>
2022-12-06 10:58:36 +0100

    Day 6 part 1

---
+++ 2022/06/solution.rb
@@ -0,0 +1,24 @@
+#  *        *        *             __o    *       *
+#   *      *       *        *    /_| _     *
+#      K  *     K      *        O'_)/ \  *    * .
+#     <')____  <')____    __*   V   \  ) __  * .
+#      \ ___ )--\ ___ )--( (    (___|__)/ /*     *
+#     *  |   |    |   |  * \ \____| |___/ /  * .
+#          |*  |    |   |    \____________/       *
+#
+#                s p o i l e r s
+
+input_path = File.expand_path(File.dirname(__FILE__)) + "/data.txt"
+
+input = File.open(input_path).read.strip.chars
+
+packet = 4
+i = 0
+
+while i < input.length
+  break packet + 1 if input[i..packet] == input[i..packet].uniq
+
+  i += 1
+  packet += 1
+end
+p packet
