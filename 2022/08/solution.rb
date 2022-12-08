9eed75ff73e0ac56ca42383cd7bcc42f510777bf
Charlotte Bory <cmmbory@gmail.com>
2022-12-08 17:26:23 +0100

    Day 8 part 2

--- 2022/08/solution.rb
+++ 2022/08/solution.rb
@@ -14,6 +14,9 @@ input = File.open(input_path).read.split("\n").map { |l| l.chars.map(&:to_i)}
 
 MAP_SIZE = {y: input.length, x: input[0].length}
 
+# *************************************** #
+# ************** P A R T   I ************ #
+# *************************************** #
 def check_from_left(input, forest)
   puts "Checking from left"
   input.each_with_index do |row, y|
@@ -102,3 +105,96 @@ end
 
 # Part 1
 p check_outside_in_view(input)
+
+
+# *************************************** #
+# ************* P A R T   II ************ #
+# *************************************** #
+
+def check_to_left(input, forest)
+  puts "Checking to left"
+  input.each_with_index do |row, y|
+    row.each_with_index do |tree, x|
+      location = [y, x].join("-")
+      count = 0
+      next forest[location] *= count if x == 0
+      row[0..x - 1].reverse.each do |height|
+        count += 1
+        break if height >= tree
+      end
+      forest[location] *= count
+    end
+  end
+
+  forest
+end
+
+def check_to_right(input, forest)
+  puts "Checking to right"
+  input.each_with_index do |row, y|
+    row.each_with_index do |tree, x|
+      location = [y, x].join("-")
+      count = 0
+      next forest[location] *= count if x == MAP_SIZE[:x]
+      row[x + 1..- 1].each do |height|
+        count += 1
+        break if height >= tree
+      end
+      forest[location] *= count
+    end
+  end
+
+  forest
+end
+
+def check_to_top(input, forest)
+  puts "Checking to top"
+  input = input.transpose
+  input.each_with_index do |row, y|
+    row.each_with_index do |tree, x|
+      location = [x, y].join("-")
+      count = 0
+      next forest[location] *= count if x == 0
+      row[0..x - 1].reverse.each do |height|
+        count += 1
+        break if height >= tree
+      end
+      forest[location] *= count
+    end
+  end
+
+  forest
+end
+
+def check_to_bottom(input, forest)
+  puts "Checking to top"
+  input = input.transpose
+  input.each_with_index do |row, y|
+    row.each_with_index do |tree, x|
+      location = [x, y].join("-")
+      count = 0
+      next forest[location] *= count if x == MAP_SIZE[:y] - 1
+      row[x + 1..- 1].each do |height|
+        count += 1
+        break if height >= tree
+      end
+      forest[location] *= count
+    end
+  end
+
+  forest
+end
+
+def plot_scenic_view(input)
+  forest = Hash.new(1)
+  forest = check_to_left(input, forest)
+  forest = check_to_right(input, forest)
+  forest = check_to_top(input, forest)
+  forest = check_to_bottom(input, forest)
+end
+
+# Part 2
+forest = plot_scenic_view(input)
+# print_map(forest)
+p forest.values.max
+
